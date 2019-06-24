e6384986c2d970a35e0dbce96ef8abff2b7d4570
Araquach <adam@contra-design.com>
2019-06-24 18:16:58 +0100

    some changes

--- main.go
+++ main.go
@@ -17,32 +17,6 @@ var (
 	tplCreate *template.Template
 )
 
-func init() {
-	var (
-		dbhost     = os.Getenv("DB_HOST")
-		dbport   = os.Getenv("DB_PORT")
-		dbuser     = os.Getenv("DB_USER")
-		dbpassword = os.Getenv("DB_PASSWORD")
-		dbname   = os.Getenv("DB_NAME")
-	)
-
-	// loads values from .env into the system
-	if err := godotenv.Load(); err != nil {
-		log.Print("No .env file found")
-	}
-
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		dbhost, dbport, dbuser, dbpassword, dbname)
-	db, err := gorm.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-	db.LogMode(true)
-
-}
-
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	if err := tplHome.Execute(w, nil); err != nil {
@@ -57,12 +31,38 @@ func create(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
 func main() {
+	var (
+		dbhost     = os.Getenv("DB_HOST")
+		dbport   = os.Getenv("DB_PORT")
+		dbuser     = os.Getenv("DB_USER")
+		dbpassword = os.Getenv("DB_PASSWORD")
+		dbname   = os.Getenv("DB_NAME")
+	)
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		dbhost, dbport, dbuser, dbpassword, dbname)
+	db, err := gorm.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	db.LogMode(true)
+
+
 
 	tplHome = template.Must(template.ParseFiles("templates/index.gohtml"))
 	tplCreate = template.Must(template.ParseFiles("templates/create.gohtml"))
